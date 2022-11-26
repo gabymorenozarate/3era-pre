fa1939e0e02350b5eac7711dc412e752415f2943
gabymorenozarate <gabrielmorenozarate@gmail.com>
2022-11-26 03:13:47 -0300

    3era pre entrega

---
+++ src/stock.js
@@ -0,0 +1,64 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Miller",
+        tipo: "Cerveza Rubia",
+        desc: "Cerveza ligera, fresca",
+        precio: 370,
+        img: 'public/images/miller.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Heineken",
+        tipo: "Cerveza Rubia",
+        desc: "Buen cuerpo, suave amargor",
+        precio: 400,
+        img: './public/images/heineken.jpg.png',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Guiness",
+        tipo: "Cerveza Negra",
+        desc: "Negra, buen cuerpo y espuma",
+        precio: 650,
+        img: 'public/images/guiness.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Peroni",
+        tipo: "Cerveza Rubia",
+        desc: "La mejor birra italiana",
+        precio: 600,
+        img: 'public/images/peroni.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Grolsch",
+        tipo: "Cerveza Rubia",
+        desc: "Cerveza con buen amargor",
+        precio: 500,
+        img: 'public/images/grolsch.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Corona",
+        tipo: "Cerveza Rubia",
+        desc: "La mas fina de todas",
+        precio: 400,
+        img: 'public/images/corona.jpg',
+        cantidad:1
+    }
+]
+
+export { productos };
